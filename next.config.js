2e191cb44dd82ccc0f2b8cf40f3b4b14f2c761a4
henry-oway <henry@oway.io>
2025-02-13 14:37:23 -0800

    more config changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     unoptimized: true, // Required for static export
   },
   basePath: process.env.NODE_ENV === 'production' ? '/onboarding-step-colors-template' : '',
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during builds
+  },
 }
 
 module.exports = nextConfig;
