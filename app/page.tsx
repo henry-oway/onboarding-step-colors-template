2aaa10949198205d910cd1e22783e9fbe33ca7e7
henry-oway <henry@oway.io>
2025-02-13 14:17:06 -0800

    deploy debug tests

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import OnboardingForm from "@/components/onboarding-form";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
-      <OnboardingForm />
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Test Deploy</h1>
+      <p>If you can see this, the deployment worked!</p>
     </div>
   );
 }
