49236a0c0f454fe03468f4857f65695e1127d99d
henry-oway <henry@oway.io>
2025-02-13 14:21:17 -0800

    another try

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import OnboardingForm from "@/components/onboarding-form";
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Test Deploy</h1>
-      <p>If you can see this, the deployment worked!</p>
+    <div className="min-h-screen bg-background">
+      <OnboardingForm />
     </div>
   );
 }
